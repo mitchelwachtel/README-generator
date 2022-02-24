2ba95deb1f1c9a176b1f4b9860604e8c5562e7e8
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-02-24 15:17:03 -0500

    wrote basic prompt questions, no files are written

--- index.js
+++ index.js
@@ -2,34 +2,57 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "What is your name?",
-//       name: "name",
-//     },
-//     {
-//       type: "input",
-//       message: "Where are you located?",
-//       name: "location",
-//     },
-//     {
-//       type: "input",
-//       message: "Provide a short bio:",
-//       name: "bio",
-//     },
-//     {
-//       type: "input",
-//       message: "What is your LinkedIn URL?",
-//       name: "linkedin",
-//     },
-//     {
-//       type: "input",
-//       message: "What is your GitHub URL?",
-//       name: "github",
-//     },
-//   ])
-//   .then((data) => {
+  .prompt([
+    {
+      type: "input",
+      message: "Provide title of the project:",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Describe the project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Provide installation instructions:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Provide usage information:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Provide contribution guidelines:",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Provide test instructions:",
+      name: "test",
+    },
+    {
+      type: "list",
+      message: "Select the license that applies to this application:",
+      name: "license",
+      choices: ["MIT", "GNU"],
+    },
+    {
+      type: "input",
+      message: "Provide GitHub username:",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "Provide your email address:",
+      name: "email",
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+  });
 //     const fileName = `${data.name}.html`;
 //     const contents = `<!DOCTYPE html> <html> <head> <title>${data.name}</title> </head> <body style="text-align: center;"> <h1>${data.name}</h1> <div style="margin: 10px; padding: 10px;"> <p>Location: ${data.location}</p> <p>LinkedIn: ${data.linkedin}</p> <p>GitHub: ${data.github}</p> <p>Bio: ${data.bio}</p> </div> </body> </html> `;
 
